32db2dee0f448ad80801efa4ea7dd4654d34eb3c
MicaelTargino <micaelgomestargino@gmail.com>
2024-11-22 19:27:32 -0300

    Add commented development base_url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://growthnessapi.targino.dev/',
+  // baseURL: 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
